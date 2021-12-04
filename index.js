9841f131fcb7e6227faae8e48caaeacca8d9b396
Filizzola <juliofilizzola@hotmail.com>
2021-12-04 14:16:01 -0300

    BUILD: criando funcionalidade de groupos

--- index.js
+++ index.js
@@ -4,21 +4,43 @@ const port = 3000;
 const path = require('path');
 const socketIo = require('socket.io');
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/grupo1', express.static(path.join(__dirname, 'public')));
+app.use('/grupo2', express.static(path.join(__dirname, 'public')));
 
 
 const serve = app.listen(port, () => console.log(`Example app listening on port %s`, port));
 
 const io = socketIo(serve);
 
-const messages = [];
+let messages = { grupo1: [], grupo2: [] };
 
-io.on('connection', (socket) => {
+const grupo1 = io.of("/grupo1").on('connection', (socket) => {
   console.log("new connection");
-  socket.emit('update_messages', messages);
+  socket.emit('update_messages', messages.grupo1);
   socket.on('new_message', (data) => {
-    console.log(data)
-    messages.push(data.msg);
-    io.emit('update_messages', messages);
+  
+    messages.grupo1.push(data);
+    grupo1.emit('update_messages', messages.grupo1);
   });
 })
+
+
+const grupo2 = io.of("/grupo2").on('connection', (socket) => {
+  console.log("new connection");
+  socket.emit('update_messages', messages.grupo2);
+  socket.on('new_message', (data) => {
+    console.log(data)
+    messages.grupo2.push(data);
+    grupo2.emit('update_messages', messages.grupo2);
+  });
+})
+
+// io.on('connection', (socket) => {
+//   console.log("new connection");
+//   socket.emit('update_messages', messages);
+//   socket.on('new_message', (data) => {
+//     console.log(data)
+//     messages.push(data);
+//     io.emit('update_messages', messages);
+//   });
+// })
--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,8 @@
+const room = window.location.pathname.replace(/\//g, '');
 
+const socket = io(`http://localhost:3000/${room}`);
 
-const socket = io('http://localhost:3000');
+let user = null;
 
 socket.on('update_messages', (messages) => {
   updateMessagesOnScreen(messages);
@@ -8,12 +10,13 @@ socket.on('update_messages', (messages) => {
 
 const updateMessagesOnScreen = (msg) => {
 
+
   const div_messages = document.querySelector("#messages");
 
   let list_messages = '<ul>';
 
-  msg.forEach(  messages => {
-    list_messages += `<li>${messages}</li>`
+  msg.forEach(({user, msg}) => {
+    list_messages += `<li> ${user}: ${msg}</li>`
   } );
   list_messages += '</ul>';
 
@@ -25,10 +28,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   form.addEventListener('submit' , (e) => {
     e.preventDefault();
+    
+    if (!user) {
+      return alert("Defina um usuario");
+    }
+
     const message = document.forms['message_form_name']['msg'].value;
     document.forms['message_form_name']['msg'].value = "";
-    socket.emit('new_message', { msg: message });
-    
-    
+    socket.emit('new_message', { user: user, msg: message });
+  } )
+
+  const userForm = document.querySelector('#user_form');
+
+  userForm.addEventListener('submit' , (e) => {
+    e.preventDefault();
+    user = document.forms['user_form_name']['user'].value;
+    userForm.parentNode.removeChild(userForm);
   } )
 })
